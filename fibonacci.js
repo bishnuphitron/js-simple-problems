ae6291e5677258c2122f03324219cff95995f3b1
bsnun8 <bishnunath018@gmail.com>
2022-06-12 16:21:48 +0600

    Handle unexpected input using simple return

--- fibonacci.js
+++ fibonacci.js
@@ -11,8 +11,29 @@
   ith = (i-1)th + (i-2)th 
 */
 
+/* 
 const fibo = [0, 1]
 for (let i = 2; i <= 10; i++) {
   fibo[i] = fibo[i - 1] + fibo[i - 2];
 }
-console.log(fibo);
+console.log(fibo); 
+*/
+
+function fibonacciSeries(num) {
+  // validation
+  if (typeof num != 'number') {
+    return 'Please give a number'
+  }
+  if (num < 2) {
+    return 'Please enter a positive number greater than 1';
+  }
+
+  const fibo = [0, 1]
+  for (let i = 2; i < num; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  return fibo;
+}
+
+const fiboSeries = fibonacciSeries(3);
+console.log(fiboSeries);
